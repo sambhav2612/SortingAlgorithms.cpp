0eb36c60513d5bc13b5226c0901ef12b1d87510a
2pac <tarasyaremabcn@gmail.com>
2017-10-25 21:07:42 +0200

    Modified the insertion sort algorithm.

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // "Insertion Sort" Sorting Algorithm Time Complexity O(N^2).
-void sort (vector<int>& listOfElements) {
+void sort (int *listOfElements, int size) {
     // This sorting method works by doing inplace comparison between it's
     // elements, basically the algorithm Assumes that from index 0 to the hole
     // all elements are sorted, after making this assumption the algorithm
@@ -11,7 +11,7 @@ void sort (vector<int>& listOfElements) {
     // in sorted subarray. Insert this element in the sorted subarray and
     // shift all the elements accordingly
 	int value = 0, hole = 0;
-	for (int i = 1; i < listOfElements.size(); ++i) {
+	for (int i = 1; i < size; ++i) {
 		value = listOfElements[i];
 		hole = i;
 
@@ -23,41 +23,42 @@ void sort (vector<int>& listOfElements) {
 
     // Print the list with the state of each iteration to simulate the Insertion sort
     // for the user
-	cout << "Iteration #" << i+1 << ":" ;
-	for (int k = 0; k < listOfElements.size(); ++k)
+/*	cout << "Iteration #" << i+1 << ":" ;
+	for (int k = 0; k < size; ++k)
 		cout << listOfElements[k];
-	cout << endl;
+	cout << endl;*/
 	}
 }
 
 int main () {
 
     // Read and declare size of desired list by user
-	int sizeOfList;
+	int sizeOfList, *listOfElements;
 	cout << "Enter Size: " << endl;
 	cin >> sizeOfList;
 
 	// Declare vector to save list of elements
-	vector<int> listOfElements(sizeOfList);
+	listOfElements = (int *)malloc(sizeOfList * sizeof(int));
 
 	// Receive list elements from the user and save them in the vector
 	cout << endl << "Enter elements: " << endl;
 	for (int i = 0; i < sizeOfList; ++i)
 	  cin >> listOfElements[i];
-
+/*
     // Print the list before sorting for the user
 	cout << endl << "Array before sorting: " << endl;
 	for (int i = 0; i < sizeOfList; ++i)
 		cout << listOfElements[i];
 	cout << endl << endl;
-
+*/
 	// Sort the elements in the list
-	sort (listOfElements);
-
+	sort (listOfElements, sizeOfList);
+	printf("done\n");
+/*
 	// Print the list after the list has been sorted
 	cout << endl << "Array after sorting: " << endl;
-	for (int i = 0; i < listOfElements.size(); ++i)
+	for (int i = 0; i < sizeOfList; ++i)
 		cout << listOfElements[i];
-	return 0;
+*/	return 0;
 
 }
