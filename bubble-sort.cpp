045af3934b1719a6ab72d08c3a94d38ba4916f38
2pac <tarasyaremabcn@gmail.com>
2017-10-25 20:20:11 +0200

    Modified bubble-sort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,24 +1,21 @@
-# include "includeAll.h"
+#include "includeAll.h"
 using namespace std;
 
 
 // "Bubble Sort" Sorting Algorithm Time Complexity O(N^2).
-void sort (vector<int>& listOfElements) {
-
-	for (int i = 0; i < listOfElements.size()-1; ++i) {
-		for (int j = i+1; j < listOfElements.size(); ++j) {
+void sort (int *listOfElements, int sizeOfList) {
+	for (int i = 0; i < sizeOfList - 1; ++i) {
+		for (int j = i+1; j < sizeOfList; ++j) 
             // If the element in the pos " i " is bigger than the element
             // in the pos " j ", we need to swap them to have the list in
             // an  ASC order, if you wish to sort the list in DESC order change the
             // comparison operator to less than " < "
-            if (listOfElements[i] > listOfElements[j]) {
+            if (listOfElements[i] > listOfElements[j]) 
                 swap (listOfElements[i], listOfElements[j]);
-                }
-		}
 		// Print the list with the state of each iteration to simulate the bubble sort
 		// For the user
-		cout << "Iteration #" << i+1 << ":" ;
-		for (int k = 0; k < listOfElements.size(); ++k)
+		cout << "Iteration #" << i + 1 << ":" ;
+		for (int k = 0; k < sizeOfList; ++k)
 			cout << listOfElements[k];
 		cout << endl;
 	}
@@ -27,12 +24,12 @@ void sort (vector<int>& listOfElements) {
 int main () {
 
   // Read and declare size of desired list by user
-	int sizeOfList;
+	int sizeOfList, *listOfElements;
 	cout << "Enter Size: " << endl;
 	cin >> sizeOfList;
 
 	// Declare vector to save list of elements
-	vector<int> listOfElements(sizeOfList);
+	listOfElements = (int *)malloc(sizeOfList * sizeof(int));
 
 	// Receive list elements from the user and save them in the vector
 	cout << endl << "Enter elements: " << endl;
@@ -46,13 +43,13 @@ int main () {
 	cout << endl << endl;
 
 	// Sort the elements in the list
-	sort (listOfElements);
+	sort (listOfElements, sizeOfList);
 
 	// Print the list after the list has been sorted
 	cout << endl << "Array after sorting: " << endl;
-	for (int i = 0; i < listOfElements.size(); ++i)
+	for (int i = 0; i < sizeOfList; ++i)
 		cout << listOfElements[i];
 
+	free(listOfElements);
 	return 0;
-
 }
