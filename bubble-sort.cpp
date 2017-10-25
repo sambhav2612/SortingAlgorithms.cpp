4009d18159c759de3eee6b40d39ac8440dc98154
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2017-10-25 11:55:35 +0530

    added comment in bubblesort.cpp

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 void sort (int array[], int size) {
-	
+	/*this function will compare all elements one by one to all the elements after it.
+	If this element is greater than other element then they both are swapped*/
 	for (int i = 0; i < size-1; ++i) {
 		for (int j = i+1; j < size; ++j) {
 				if (array[i] > array[j]) {
 					swap (&array[i], &array[j]);
 			}
 		}
+		/* here we check the array after each modification*/
 		cout << "Iteration #" << i+1 << ":" ;
 		for (int k = 0; k < size; ++k)
 			cout << array[k];
@@ -32,7 +34,7 @@ int main () {
 		cout << array[i];
 	cout << endl;
 	
-	sort (array, size);
+	sort (array, size);//here array is sorted by sort() function
 	
 	cout << endl << "Array after sorting: " << endl;
 	for (int i = 0; i < size; ++i)
